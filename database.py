5a58acbbec14cf89061f2d0d9b6d2a3d85a7077f
syed hussnain tahir sherazi <hussnaintahir.uk@gmail.com>
2024-04-22 22:33:56 +0100

    forming the database model for login and signup functionality

--- database.py
+++ database.py
@@ -1,2 +1,7 @@
 import sqlalchemy
 
+
+DATABASE_URL= 'postgresql://username:hussnaintahir13@host/postgres'
+engine=sqlalchemy.create_engine(DATABASE_URL)
+Base=sqlalchemy.declarative_base
+SessionLocal =sqlalchemy.sessionmaker(bind=engine,expire_on_commit=False)
