162a286492f2f041fd525a16bd10d2e157134410
syed hussnain tahir sherazi <hussnaintahir.uk@gmail.com>
2024-04-24 08:41:19 +0100

    Backend, frontend and Tokenized login merged

---
+++ Frontend/src/pages/components/VoiceAssistant/VoiceAssistantAvatar/VoiceAssistantAvatar.component.tsx
@@ -0,0 +1,8 @@
+
+
+const VoiceAssistantAvatar = ()=>{
+    return <img style={{width:"50%", height:"auto"}} src="https://cdn.pixabay.com/photo/2023/05/24/17/49/ai-generated-8015425_1280.jpg" />
+}
+
+
+export default VoiceAssistantAvatar
